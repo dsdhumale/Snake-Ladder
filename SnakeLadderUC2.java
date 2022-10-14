e6d8c99207f84b1f8fefc51cf81adc8976bd8ec9
dsdhumale <d.sdhumale@gmail.com>
2022-10-14 22:49:35 +0530

    Add : Feature of rolling dice

---
+++ SnakeLadderUC2.java
@@ -0,0 +1,10 @@
+package snakeLadderUC;
+
+public class SnakeLadderUC2 {
+	public static void main(String[] args) {
+		System.out.println("Start The Game");
+		int dicecheck = (int) (Math.random() * 6) + 1;
+		System.out.println("Dice thrown no is " + dicecheck);
+	}
+
+}
