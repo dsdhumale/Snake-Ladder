8da664692a81693835a69ba14b4b8662c0ea9c63
dsdhumale <d.sdhumale@gmail.com>
2022-10-14 22:45:09 +0530

    Add : Game start from starting position

---
+++ SnakeLadderUC1.java
@@ -0,0 +1,12 @@
+package snakeLadderUC;
+
+public class SnakeLadderUC1 {
+	static int player = 0;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake Ladder Program");
+		System.out.println(" Player is at position :" + player);
+
+	}
+
+}
