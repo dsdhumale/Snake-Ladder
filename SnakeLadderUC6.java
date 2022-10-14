6722f6de7fb7c3820ea8d3eeab02080d3972a851
dsdhumale <d.sdhumale@gmail.com>
2022-10-15 00:23:31 +0530

    Add : Feature to display count after how many step required to win d game

---
+++ SnakeLadderUC6.java
@@ -0,0 +1,34 @@
+package snakeLadderUC;
+
+public class SnakeLadderUC6 {
+	public static void main(String[] args) {
+
+		int player = 0;
+		int count = 0;
+		System.out.println("Start The Game");
+		while (player < 100) {
+			count++;
+			int optionCheck = (int) (Math.random() * 3) + 1;
+			int dicecheck = (int) (Math.random() * 6) + 1;
+			if (optionCheck == 1) {
+				System.out.println("No play");
+			} else if (optionCheck == 2) {
+				player += dicecheck;
+				if (player > 100) {
+					player -= dicecheck;
+				} else if (player == 100) {
+					System.out.println(" Player Win the Game and position is " + player);
+
+				}
+				System.out.println("Ladder !!! player position is " + player);
+			} else if (optionCheck == 3) {
+				player -= dicecheck;
+				if (player <= 0) {
+					player = 0;
+				}
+				System.out.println("Snake !!! player position is " + player);
+			}
+		}
+		System.out.println("Count for winning position is " + count);
+	}
+}
